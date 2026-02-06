3f9e6396df67b1438a932df993fdd6d9247c7cf8
Abi-390 <dekaabinash07@gmail.com>
2026-02-06 21:35:49 +0530

    Updated db js

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log(" Connected to MongoDB");
+    console.log(" Connected to MongoDB successfully");
     return mongoose.connection;
   } catch (err) {
     console.error(" Error connecting to MongoDB:", err.message);
