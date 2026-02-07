14163eb1ea0b88244e0976e719dfac783eec37e7
Abi-390 <dekaabinash07@gmail.com>
2026-02-07 22:02:13 +0530

    Event model added

---
+++ src/models/eventModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  date: {
+    type: Date,
+    required: true
+  },
+  location: String,
+  club: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Club',
+    required: true
+  },
+  attendees: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
